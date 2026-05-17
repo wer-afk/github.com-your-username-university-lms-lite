f08f5fb51e643c52164c148a67f70c0207e23cb7
Sherzod <inoyatovsher14@gmail.com>
2026-05-17 17:44:38 +0500

    Add Docker Compose and Nginx deployment setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/login", {
+      const response = await fetch("/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -82,7 +82,7 @@ function Register() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/register", {
+      const response = await fetch("/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
